e92903f5547dc954dd84efca1ed730c81cfb9c9c
Jerzy Drozdz <jerzy.drozdz@gmail.com>
2018-01-28 19:08:28 +0100

    Old app moved to legacy branch

--- ughjobs/controllers/static_controller.py
+++
@@ -1,7 +0,0 @@
-from flask import send_from_directory
-
-def index_get():
-    return send_from_directory('static/', 'index.html')
-
-def app_get(path):
-    return send_from_directory('static/', path )
--- ughjobs/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-from __future__ import absolute_import
-# import models into model package
-from .api_response import ApiResponse
-from .application import Application
-from .company import Company
-from .job import Job
-from .user import User
